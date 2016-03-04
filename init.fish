032a257d3b1ce5fba231ca0698f48e43e13385a9
Bruno Pinto <brunoferreirapinto@gmail.com>
2016-03-04 12:30:53 +0000

    Load event-based function on init

--- init.fish
+++ init.fish
@@ -2,4 +2,9 @@ function init -a path --on-event init_z
   if test -z $Z_SCRIPT_PATH
     echo "Please install 'z' first!"; return 1
   end
+
+  function z.check --on-variable PWD --description 'Setup z on directory change'
+    status --is-command-substitution; and return
+    bash -c "source $Z_SCRIPT_PATH; _z --add `pwd -P`"
+  end
 end
