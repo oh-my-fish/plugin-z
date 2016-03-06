4526b7fbcef146ae140cdbfe731923d5b8b31795
Justin Hileman <justin@justinhileman.info>
2016-03-06 13:05:07 -0800

    Initialize immediately, rather than waiting for an event.

--- init.fish
+++ init.fish
@@ -1,8 +1,6 @@
-function init -a path --on-event init_z
-  if test -z $Z_SCRIPT_PATH
-    echo "Please install 'z' first!"; return 1
-  end
-
+if test -z $Z_SCRIPT_PATH
+  echo "Please install 'z' first!"
+else
   function z.check --on-variable PWD --description 'Setup z on directory change'
     status --is-command-substitution; and return
     bash -c "source $Z_SCRIPT_PATH; _z --add `pwd -P`"
