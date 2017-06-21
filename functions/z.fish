ea4ddfcc8194da204aaa4a2861308bdaa7f4d2c7
Stephen M. Coakley <me@stephencoakley.com>
2017-06-21 08:59:24 -0500

    Improve script readability

--- functions/z.fish
+++ functions/z.fish
@@ -2,13 +2,13 @@ function z -d "jump around"
   set -lx Z_SCRIPT_PATH (dirname (status -f))/../z/z.sh
 
   # Start a Bash process, source z, run the _z function, and capture the working directory and exit status.
-  bash -c "
-    source \$Z_SCRIPT_PATH
-    _z \"\$@\" 2>&1
-    Z_STATUS=\$?
-    echo \"\$PWD\" >&2
-    exit \$Z_STATUS
-  " bash $argv ^| read -l Z_PWD
+  bash -c '
+    source $Z_SCRIPT_PATH
+    _z "$@" 2>&1
+    Z_STATUS=$?
+    echo "$PWD" >&2
+    exit $Z_STATUS
+  ' bash $argv ^| read -l Z_PWD
   set -l Z_STATUS $status
 
   # If z changed directories, reflect that in the current process.
